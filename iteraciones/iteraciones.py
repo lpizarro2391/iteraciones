d633c800543708cf81012562868a211c2180ccf9
lpizarro2391 <lourdespizarro@gmail.com>
2020-04-12 16:24:55 -0600

    optimizacion prescindir de variable buleana, usando ciclo for y sentencia break

--- iteraciones/iteraciones.py
+++ iteraciones/iteraciones.py
@@ -2,11 +2,9 @@ inf=int(input("limite inferior del intervalo:"))
 sup=int(input("limite superior del intervalo:"))
 print("Los números primos entre", inf, "y",sup,"son")
 for num in range(inf,sup+1):
-    es_primo=True
-    i=2
-    while (es_primo and i<num):
-        es_primo=(num%i)!=0
-        i+=1
-    if es_primo:
-        print(num,"es un numero primo")
+    for i in range(2,num):
+        if (num%1)==0:
+            break
+        elif i==num-1:
+            print(num,"es un numero primo")
     
