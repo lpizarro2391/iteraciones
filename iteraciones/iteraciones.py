0181b840036f410905c1698c6bdad7d342e86807
lpizarro2391 <lourdespizarro@gmail.com>
2020-04-12 15:40:35 -0600

    el ciclo while finaliza cuando encontramos un divisor de num, o probamos con todos os valores entre 2  num -1.

--- iteraciones/iteraciones.py
+++ iteraciones/iteraciones.py
@@ -4,3 +4,6 @@ print("Los números primos entre", inf, "y",sup,"son")
 for num in range(inf,sup+1):
     es_primo=True
     i=2
+    while (es_primo and i<num):
+        es_primo=(num%i)!=0
+        i+=1
